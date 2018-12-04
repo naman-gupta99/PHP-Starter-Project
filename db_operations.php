3915dc39fd25e2a7a871b4084d1bb693aa9cf0e7
naman-gupta99 <kungfunaman@gmail.com>
2018-12-04 22:59:02 +0530

    Minor errors removed

--- db_operations.php
+++ db_operations.php
@@ -2,15 +2,18 @@
 if(isset($_POST['submit_create']))
 {
   include_once 'db_conn.php';
+  $goback = "<br /><a href='index.php'><button>GO BACK</button></a>";
   $name = mysqli_real_escape_string($conn, $_POST['name']);
   $value = mysqli_real_escape_string($conn, $_POST['value']);
   if(empty($name) || empty($value))
   {
     $message = "Empty Fields";
+    echo $message;
+    echo $goback;
   }
   else {
     $message = "Success";
+    echo $message;
+    echo $goback;
   }
-  echo "<script type='text/javascript'>alert('$message');</script>";
-  header("Location: index.php?create=empty_fields");
 }
